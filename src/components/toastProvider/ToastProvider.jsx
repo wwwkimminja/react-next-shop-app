23b4460ae09ecf0973ea4ac97734afcc2814d370
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2023-08-12 12:27:18 +0900

    feat : ToastProvider

--- src/components/toastProvider/ToastProvider.jsx
+++ src/components/toastProvider/ToastProvider.jsx
@@ -1,8 +1,13 @@
+'use client'
 import React from 'react'
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 function ToastProvider() {
   return (
-    <div>ToastProvider</div>
+   <ToastContainer
+   autoClose={2000}
+   />
   )
 }
 
