5913032299216a77ff1bed84491c775e35ec9ea0
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2023-08-12 13:19:07 +0900

    fix

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,10 +13,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ToastProvider>
+        <ToastProvider/>
         {children}
-        </ToastProvider>
-        </body>
+      </body>
     </html>
   )
 }
